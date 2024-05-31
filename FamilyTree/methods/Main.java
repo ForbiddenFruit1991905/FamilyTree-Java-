9fc2544306caaa1c3f3270919ff1c2a43793b70e
Лихачёва Виктория Михайловна <29436838@s.masi.ru>
2024-05-31 03:09:31 +0300

    NEW_fixed

--- FamilyTree/methods/Main.java
+++ FamilyTree/methods/Main.java
@@ -3,7 +3,7 @@ package HW.familyTree.FamilyTree.methods;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-//import java.util.Scanner;
+import java.util.Scanner;
 import HW.familyTree.FamilyTree.HumanInfo.Human;
 import HW.familyTree.FamilyTree.enums.Gender;
 import HW.familyTree.FamilyTree.enums.Relation;
@@ -40,68 +40,12 @@ public class Main {
         person4.addKinder(person9);
         person6.addKinder(person10);
 
-
-//        Scanner scanner = new Scanner(System.in);
-//        while (true) {
-//            getCommands();
-//            String command = scanner.nextLine();
-//            if (command.equals("1")) {
-//                System.out.println("Укажите id:");
-//                String id = String.valueOf(Integer.parseInt(scanner.nextLine()));
-//                System.out.println("Укажете имя:");
-//                String firstname = scanner.nextLine();
-//                System.out.println("Укажете отчество:");
-//                String middlename = scanner.nextLine();
-//                System.out.println("Укажете фамилию:");
-//                String lastname = scanner.nextLine();
-//                System.out.println("Введите Male или Female");
-//                Gender gender = Gender.valueOf(scanner.nextLine());
-//                System.out.println("Укажите тип родственной связи Spouses/Child/Divorced:");
-//                Relation.Type relation = Relation.Type.valueOf(scanner.nextLine());
-//                System.out.println("Запись внесена в семейное древо ---> " + " id: " + id + ", имя: " + firstname + ", отчество: " + middlename + ", фамилия: " + lastname + ", пол: " + gender + ", семейная связь: " + relation);
-//                familyTree.addHuman(new Human(Integer.parseInt(id), firstname, middlename, lastname, gender, relation));
-//            } else if (command.equals("2")) {
-//                System.out.println(familyTree);
-//            } else if (command.equals("3")) {
-//                System.out.println("Введите ID: ");
-//                int input = scanner.nextInt();
-//                System.out.println(familyTree.findHumanById(input));
-//            } else if (command.equals("4")) {
-//                System.out.println("Введите ID для удаления: ");
-//                int del_human = scanner.nextInt();
-//                familyTree.removeHuman(del_human);
-//                System.out.println("Запись удалена");
-//            } else if (command.equals("quit")) {
-//                break;
-//            } else if (command.equals("save")) {
-//                try {
-//                    familyTree.saveToFile("tree.txt");
-//                    System.out.println("Семейное древо сохранено.txt");
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            } else if (command.equals("load")) {
-//                try {
-//                    FamilyTree loadedFamilyTree = FamilyTree.loadFromFile("tree.txt");
-//                    System.out.println("Произведена выгрузка семейного древа \n" + loadedFamilyTree);
-//                } catch (IOException | ClassNotFoundException e) {
-//                    e.printStackTrace();
-//                }
-//            } else {
-//                System.out.println("Такой команды нет");
-//            }
-//        }
-//        scanner.close();
-
-//           Сохранение дерева в файл
         try {
             familyTree.saveToFile("tree.txt");
             System.out.println("Семейное древо сохранено.txt");
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-//        Загрузка дерева из файла
         try {
             FamilyTree loadedFamilyTree = FamilyTree.loadFromFile("tree.txt");
             System.out.println("Произведена выгрузка семейного древа \n" + loadedFamilyTree);
@@ -109,19 +53,83 @@ public class Main {
             e.printStackTrace();
         }
 
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            getCommands();
+            String command = scanner.nextLine();
+            if (command.equals("1")) {
+                System.out.println("Укажите id:");
+                String id = String.valueOf(Integer.parseInt(scanner.nextLine()));
+                System.out.println("Укажете имя:");
+                String firstname = scanner.nextLine();
+                System.out.println("Укажете отчество:");
+                String middlename = scanner.nextLine();
+                System.out.println("Укажете фамилию:");
+                String lastname = scanner.nextLine();
+                System.out.println("Введите Male или Female");
+                Gender gender = Gender.valueOf(scanner.nextLine());
+                System.out.println("Укажите тип родственной связи Spouses/Child/Divorced:");
+                Relation.Type relation = Relation.Type.valueOf(scanner.nextLine());
+                System.out.println("Запись внесена в семейное древо ---> " + " id: " + id + ", имя: " + firstname + ", отчество: " + middlename + ", фамилия: " + lastname + ", пол: " + gender + ", семейная связь: " + relation);
+                familyTree.addHuman(new Human(Integer.parseInt(id), firstname, middlename, lastname, gender, relation));
+                try {
+                    familyTree.saveToFile("tree.txt");
+                    System.out.println("Семейное древо сохранено.txt");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    FamilyTree loadedFamilyTree = FamilyTree.loadFromFile("tree.txt");
+                    System.out.println("Произведена выгрузка семейного древа \n" + loadedFamilyTree);
+                } catch (IOException | ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            } else if (command.equals("2")) {
+                System.out.println(familyTree);
+            } else if (command.equals("3")) {
+                System.out.println("Введите ID: ");
+                int input = scanner.nextInt();
+                System.out.println(familyTree.findHumanById(input));
+            } else if (command.equals("4")) {
+                System.out.println("Введите ID для удаления: ");
+                int del_human = scanner.nextInt();
+                familyTree.removeHuman(del_human);
+                System.out.println("Запись удалена");
+            } else if (command.equals("quit")) {
+                break;
+            } else {
+                System.out.println("Такой команды нет");
+            }
+        }
+        scanner.close();
+
+////           Сохранение дерева в файл
+//        try {
+//            familyTree.saveToFile("tree.txt");
+//            System.out.println("Семейное древо сохранено.txt");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+////        Загрузка дерева из файла
+//        try {
+//            FamilyTree loadedFamilyTree = FamilyTree.loadFromFile("tree.txt");
+//            System.out.println("Произведена выгрузка семейного древа \n" + loadedFamilyTree);
+//        } catch (IOException | ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+
 
     }
 
-//    private static void getCommands() {
-//        System.out.println("Выберите из списка необходимую комманду: ");
-//        System.out.println("1 - добавить запись в семейное древо");
-//        System.out.println("2 - показать весь список");
-//        System.out.println("3 - найти человека по ID");
-//        System.out.println("4 - удалить запись из семейного древа");
-//        System.out.println("save - сохранить запись");
-//        System.out.println("load - загрузка записей");
-//        System.out.println("quit - выход из приложения");
-//    }
+    private static void getCommands() {
+        System.out.println("Выберите из списка необходимую комманду: ");
+        System.out.println("1 - добавить запись в семейное древо");
+        System.out.println("2 - показать весь список");
+        System.out.println("3 - найти человека по ID");
+        System.out.println("4 - удалить запись из семейного древа");
+        System.out.println("quit - выход из приложения");
+    }
 }
 
 
